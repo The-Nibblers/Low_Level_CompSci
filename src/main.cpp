02a02f154e9ecece85fca33a3e76bd04794b8c18
Jens Ouwens <99173882+The-Nibblers@users.noreply.github.com>
2025-09-27 15:46:09 +0200

    Added: physics update profiler. reordered

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,6 @@ int main() {
     sf::Clock deltaClock;
     int iterations = 100000;
 
-    sf::Vector2u windowSize = window.getSize();
-
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
             ImGui::SFML::ProcessEvent(window, *event);
@@ -29,23 +27,26 @@ int main() {
                 window.close();
         }
 
+        sf::Vector2u windowSize = window.getSize();
+
         sf::Time deltaTime = deltaClock.restart();
         float deltatime = deltaTime.asSeconds();
         ImGui::SFML::Update(window, deltaTime);
 
-        game->updateBalls( windowSize , deltatime);
-
         {
-            PROFILE(profiler, "ImGui Interface");
-
+            //imgui show profiler
             ImGui::Begin("Controls");
-            ImGui::SliderInt("Dummy Iterations", &iterations, 1000, 10000000);
             if (ImGui::Button("Clear Profiler History")) {
                 profiler.clear();
             }
             ImGui::End();
+        }
 
-            profiler.renderImGui();
+        profiler.renderImGui();
+
+        {
+            PROFILE(profiler, "Physics update");
+            game->updateBalls( windowSize , deltatime);
         }
 
         {
